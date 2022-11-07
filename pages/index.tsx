4d59b2f3bd0c85cd7df8c4ee053f275339827e38
Ricy137 <1932353948@qq.com>
2022-11-07 23:52:34 +0800

    feat: initialize home page

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import PlanetBg from '../public/images/bg/planetBg.png';
+import CNSLogo from '../public/images/logos/logo.svg';
+import BlackHoleBg from '../public/images/bg/blackHoleBg.svg';
+import AnyWeb from '../public/images/icons/anyWeb.svg';
+import Scan from '../public/images/icons/scan.svg';
+import Fluent from '../public/images/icons/fluent.svg';
+import CloudBg from '/public/images/bg/cloudBg.png';
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <div className='homeBg'>
+        <div className='homeWrapper flex flex-col items-center'>
+          <Image src={CNSLogo} alt='cns logo' className='homeHeader w-16 h-12 z-10 absolute top-[25px] left-[61px]' />
+          <div className='relative flex justify-center items-center'>
+            <Image src={PlanetBg} alt='background bg' className='w-full min-h-screen z-0' />
+            <div className='absolute text-white text-6xl text-center'>
+              <h1>你的Web3身份</h1>
+              <h1 className='text-[105px]'>将从<span className='text-[#BFB5FF]'>.web3</span>开始</h1>
+            </div>
+          </div>
+          <div className='mt-20 text-2xl flex flex-col items-center text-[#7A7880]'>
+            <p className='text-[40px] text-[#BFB5FF] mb-3'>Web3 用户名</p>
+            <p>用可读性更好的字符串，作为 Web3 的标识符</p>
+            <p>在 Web3 宇宙空间中遨游</p>
+          </div>
+          <div className='mt-5 mb-10 relative flex flex-col justify-center items-center w-9/12'>
+            <Image src={BlackHoleBg} alt='black hole background' className='z-0 w-full' />
+            <div className='absolute grid grid-cols-3 place-content-around w-7/12'>
+              <Image src={AnyWeb} alt='Anyweb Icon' className='z-10 w-full max-w-[172px]' />
+              <Image src={Scan} alt='Scan Icon' className='z-10 w-full max-w-[172px]' />
+              <Image src={Fluent} alt='Fluent Icon' className='z-10 w-full max-w-[172px]' />
+            </div>
+          </div>
+          <Image src={CloudBg} alt='cloud background' className='w-full' />
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Home
