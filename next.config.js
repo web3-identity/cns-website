08656fe5f6a9b17d5f9b6c2f56e261239ec9227d
Ricy137 <1932353948@qq.com>
2022-11-09 19:44:57 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
  const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 }
 
 module.exports = nextConfig
